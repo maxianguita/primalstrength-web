fd9546116c1ecb1b8b51a3f680f59525a808d419
maxianguita <maxi.7anguita@gmail.com>
2025-10-02 14:45:58 -0300

    cambios en movil

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home"; 
 import Plani from "./pages/Plani";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/planificaciones" element={<Plani />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
-
-export default App;
